43dca13a56670cd00ab4e8587f72f28c249a8fd8
seita ryutaro <dokusyoneko@outlook.jp>
2025-12-06 13:47:31 +0900

    メール認証画面css完了

--- src/resources/views/auth/verify-guide.blade.php
+++ src/resources/views/auth/verify-guide.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="/css/verify.css">
+@endsection
+
 @section('content')
-<div class="container" style="max-width:640px;">
-    <h1 class="mb-3">メール認証が必要です</h1>
-    <p class="mb-4">
-        登録していただいたメールアドレスに認証メールを送付しました。<br>
-        メール認証を完了してください。
-    </p>
-    <a href="http://localhost:8025/#" class="btn btn-primary mb-3" target="_blank">
-        認証はこちらから
-    </a>
+<div class="verify__content">
+    <div class="verify__content__inner">
+        <h1 class="mb-3">メール認証が必要です</h1>
+        <p class="mb-4">
+            登録していただいたメールアドレスに認証メールを送付しました。<br>
+            メール認証を完了してください。
+        </p>
+        <div class="verify__check">
+                <a href="http://localhost:8025/#" class="verify__check__inner" target="_blank">
+                    認証はこちらから
+                </a>
+        </div>
     <form method="POST" action="{{ route('verification.send') }}">
         @csrf
-        <button type="submit" class="btn btn-link">認証メールを再送する</button>
+        <button type="submit" class="mail__resend__button">認証メールを再送する</button>
     </form>
-    @if (session('status') === 'verification-link-sent')
-        <div class="alert alert-success mt-3">
-            認証メールを再送しました。メールをご確認ください。
-        </div>
-    @endif
+    </div>
 </div>
 @endsection
 
